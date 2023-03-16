ebeed85e18b05483ce77a05aaff00927adc88fc3
WlesterID <107825803+Mohazm@users.noreply.github.com>
2023-03-16 17:24:54 +0700

    Add files via upload

---
+++ TugasMandiri-3/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//url salam 
+Route::get('/salam',function(){
+    return "Assalamu'alaikum selamat Pagi Dunia";
+});
+
+//Route/Mahasantri
+Route::get('/kabar', function () {
+    return view('latihan.kondisi');
+});
+
+Route::get( 
+    '/mahasiswa', 
+    [MahasiswaController::class, 'dataMahasiswa'] 
+    );
+Auth::routes();
+
+//Home 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
